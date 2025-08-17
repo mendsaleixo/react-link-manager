b1f608337974e9bd92740054b53848fa8cca391b
Mendelson Aleixo <mendelson.aleixo@gmail.com>
2025-08-17 10:08:11 -0300

    Refac: chama o linkItem no LinkList.

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,14 +1,9 @@
+import LinkItem from "./LinkItem";
+
 function LinkList({ links }) {
   return (
     <ul>
-      {links.map((link) => (
-        <li key={link.id}>
-          <a href={link.url} target="_blank" rel="noopener noreferrer">
-            {link.titulo}
-          </a>
-          <span> ({link.categoria})</span>
-        </li>
-      ))}
+      <LinkItem key={link.id} link={link} />
     </ul>
   );
 }
